e588a015eda95198430b8a53fca11d206eaf29ba
Marc Made It <spurdo.dk@gmail.com>
2024-02-24 23:10:29 +0100

    e

--- api/index.js
+++ api/index.js
@@ -1,4 +1,3 @@
-// api/index.js
 import express from 'express';
 import cors from 'cors';
 import mongoose from 'mongoose';
@@ -6,46 +5,46 @@ import cookieParser from 'cookie-parser';
 import dotenv from 'dotenv';
 import session from 'express-session';
 
-import authRoutes from "./routes/auth.routes.js";
-import userRoutes from "./routes/user.routes.js";
-import blogRoutes from "./routes/blog.routes.js";
+import authRoutes from "./routes/auth.routes.js"
+import userRoutes from "./routes/user.routes.js"
+import blogRoutes from "./routes/blog.routes.js"
 
 dotenv.config();
 
-export default async (req, res) => {
-    const app = express(); // Create a new express app for each request
+const app = express();
 
-    const corsOptions = {
-        credentials: true,
-        origin: ["https://marcmadeit.vercel.app"],
-        methods: ["GET", "POST", "PUT", "DELETE"],
-        allowedHeaders: ["Content-Type", "Authorization"],
-        exposedHeaders: ["Access-Control-Allow-Private-Network"],
-    };
+const corsOptions = {
+    credentials: true,
+    origin: ["http://localhost:5173", "https://marcmadeit.vercel.app"],
+    methods: ["GET", "POST", "PUT", "DELETE"],  // Specify the actual HTTP methods here
+    allowedHeaders: ["Content-Type", "Authorization"],
+    exposedHeaders: ["Access-Control-Allow-Private-Network"],
+};
 
-    app.use(cors(corsOptions));
+app.use(cors(corsOptions));
 
-    app.use(session({
-        secret: process.env.SESSION_SECRET,
-        resave: false,
-        saveUninitialized: true,
-        cookie: {
-            secure: true,
-            sameSite: 'lax',
-            maxAge: 24 * 60 * 60 * 1000,
-        },
-        store: new session.MemoryStore(),
-    }));
+app.use(session({
+    secret: process.env.SESSION_SECRET,
+    resave: false,
+    saveUninitialized: true,
+    cookie: {
+        secure: true,
+        sameSite: 'lax',
+        maxAge: 24 * 60 * 60 * 1000,
+    },
+    store: new session.MemoryStore(),
+}));
 
-    app.use(express.json());
-    app.use(cookieParser(process.env.JWT_SECRET));
+app.use(express.json());
+app.use(cookieParser(process.env.JWT_SECRET));
 
-    app.use("/api/auth", authRoutes);
-    app.use("/api/user", userRoutes);
-    app.use('/api/blog', blogRoutes);
+app.use("/api/auth", authRoutes);
+app.use("/api/user", userRoutes);
+app.use('/api/blog', blogRoutes);
 
-    mongoose.set('strictQuery', true);
+mongoose.set('strictQuery', true);
 
+const connectToMongo = async () => {
     try {
         await mongoose.connect(process.env.MONGO_URL);
         console.log('Connected to MongoDB');
@@ -53,16 +52,11 @@ export default async (req, res) => {
         console.error('Error connecting to MongoDB:', error);
         throw error;
     }
-
-    // Wrap the express app handling in a promise
-    return new Promise((resolve, reject) => {
-        app(req, res, (error) => {
-            if (error) {
-                reject(error);
-            } else {
-                resolve();
-            }
-        });
-    });
 };
 
+
+const PORT = process.env.PORT || 8000;
+app.listen(PORT, () => {
+    connectToMongo();
+    console.log(`Server is running on port ${PORT}`);
+});
