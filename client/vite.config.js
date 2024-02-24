68439b3cb71c8d28d5d1ce3e1aa90ebad4b561e0
Marc Made It <spurdo.dk@gmail.com>
2024-02-24 20:20:27 +0100

    ere

--- client/vite.config.js
+++ client/vite.config.js
@@ -4,8 +4,4 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  define: {
-    'VITE_API_BASE_URL': '"https:/marcmadeit.vercel.app/api"',
-
-  },
 })
