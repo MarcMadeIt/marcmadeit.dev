22563b226b1d29718ac92e98e96e534d5d9884b9
Marc Made It <spurdo.dk@gmail.com>
2024-02-24 23:26:21 +0100

    sxa

--- api/controllers/user.control.js
+++ api/controllers/user.control.js
@@ -1,7 +1,7 @@
 import jwt from 'jsonwebtoken';
 import dotenv from 'dotenv';
 import User from '../modules/User.js';
-import bcrypt from 'bcryptjs';
+import bcrypt from "bcrypt";
 dotenv.config();
 
 
