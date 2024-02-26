ab819e3330c970d200be03d540c784d8ee01a8bd
Marc Made It <spurdo.dk@gmail.com>
2024-02-26 11:31:00 +0100

    Update index.js

--- api/index.js
+++ api/index.js
@@ -3,7 +3,6 @@ import cors from 'cors';
 import mongoose from 'mongoose';
 import cookieParser from 'cookie-parser';
 import dotenv from 'dotenv';
-import session from 'express-session';
 import authRoutes from "./routes/auth.routes.js";
 import userRoutes from "./routes/user.routes.js";
 import blogRoutes from "./routes/blog.routes.js";
@@ -22,17 +21,6 @@ const corsOptions = {
 
 app.use(cors(corsOptions));
 
-app.use(session({
-    secret: "klfsgkefdoeiewoio33i2ohuo3i",
-    resave: false,
-    saveUninitialized: true,
-    cookie: {
-        secure: true,
-        sameSite: 'lax',
-        maxAge: 24 * 60 * 60 * 1000,
-    },
-    store: new session.MemoryStore(),
-}));
 
 app.use(express.json());
 app.use(cookieParser(process.env.JWT_SECRET));
