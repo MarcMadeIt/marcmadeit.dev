cd8d6c2afd4a27be4bd7f98eb84f3f63a9b18e3e
Marc Made It <spurdo.dk@gmail.com>
2024-03-03 17:42:20 +0100

    Update index.js

--- api/index.js
+++ api/index.js
@@ -369,7 +369,7 @@ app.post("/api/auth/register", async (req, res) => {
 
 
 
-app.post("/api/auth/login", async (req, res) => {
+app.post("/api/auth/login", cache('20 minutes'), async (req, res) => {
     try {
         await connectToMongo();
         console.log('Before login process');
