c551e9b762229b6706513cb70ffa773649b87973
Marc Made It <spurdo.dk@gmail.com>
2024-07-19 12:20:24 +0200

    Update Projects.jsx

--- client/src/pages/projects/Projects.jsx
+++ client/src/pages/projects/Projects.jsx
@@ -9,6 +9,7 @@ const apiUrl = import.meta.env.VITE_API_BASE_URL;
 
 function Projects() {
   const [projects, setProjects] = useState([]);
+  const [loading, setLoading] = useState(false);
 
   useEffect(() => {
     const fetchProjects = async () => {
