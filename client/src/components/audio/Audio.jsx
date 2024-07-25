d69b6aa74ce00e0de338443aef226ab5b3f4b52e
Marc Made It <spurdo.dk@gmail.com>
2024-07-26 00:59:35 +0200

    update

--- client/src/components/audio/Audio.jsx
+++ client/src/components/audio/Audio.jsx
@@ -1,16 +1,40 @@
-import React from "react";
+import React, { useState } from "react";
 import "./Audio.scss";
+import { FaBackward, FaForward, FaPause, FaPlay } from "react-icons/fa6";
 
 function Audio({ src, alt = "", ...rest }) {
+  const [isPlaying, setIsplaying] = useState(false);
+
+
+  const togglePlayPause = () => {
+    setIsplaying(!isPlaying)
+  }
+
+
+
   src =
     src && src.includes("https://")
       ? src
       : `http://localhost:8000/api/podcast/get/${src}`;
 
   return (
-    <audio controls autoPlay>
-      <source {...rest} src={src} alt={alt}  type="audio/mpeg" />
-    </audio>
+    <div className="audio">
+      <audio {...rest} src={src} alt={alt}  type="audio/mpeg" />
+      <button><FaBackward /> <span>15</span></button>
+      <button onClick={togglePlayPause} className="play-pause-btn">{isPlaying ? <FaPause /> : <FaPlay className="play" /> } </button>
+      <button><span>15</span><FaForward /></button>
+
+      {/* current time */}
+      <div className="current">0:00</div>
+
+      {/* Progress bar */}
+      <div>
+        <input type="range" className="progress" />
+      </div>
+
+      {/* duration */}
+      <div className="duration">2:49</div>
+    </div>
   )
 }
 
--- client/src/function/audioplayer/AudioPlayer.jsx
+++ client/src/function/audioplayer/AudioPlayer.jsx
@@ -6,12 +6,16 @@ const AudioPlayer = ({ podcast }) => {
   return (
     <div className="audio-player">
       <div className="audio-player-content">
+        <div className="audio-player-left">
         <img src={podcast.imageinfo} alt={podcast.title} />
         <div className="audio-player-details">
           <h3>{podcast.title}</h3>
           <p>{podcast.desc}</p>
         </div>
+        </div>
+        <div className="audio-player-right">
         <Audio src={podcast.audioinfo} />
+        </div>
       </div>
     </div>
   );
