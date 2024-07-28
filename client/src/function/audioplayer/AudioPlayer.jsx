c5d21d3d10ee2a58769e4eb13a72119240fa6556
Marc Made It <spurdo.dk@gmail.com>
2024-07-28 23:06:49 +0200

    update

--- client/src/function/audioplayer/AudioPlayer.jsx
+++ client/src/function/audioplayer/AudioPlayer.jsx
@@ -1,20 +1,26 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import "./AudioPlayer.scss";
 import Audio from "../../components/audio/Audio";
 
 const AudioPlayer = ({ podcast }) => {
+  const [isVisible, setIsVisible] = useState(false);
+
+  useEffect(() => {
+    setIsVisible(true);
+  }, []);
+
   return (
-    <div className="audio-player">
+    <div className={`audio-player ${isVisible ? "show" : ""}`}>
       <div className="audio-player-content">
         <div className="audio-player-left">
-        <img src={podcast.imageinfo} alt={podcast.title} />
-        <div className="audio-player-details">
-          <h3>{podcast.title}</h3>
-          <p>{podcast.desc}</p>
-        </div>
+          <img src={podcast.imageinfo} alt={podcast.title} />
+          <div className="audio-player-details">
+            <h3>{podcast.title}</h3>
+            <p>{podcast.desc}</p>
+          </div>
         </div>
         <div className="audio-player-right">
-        <Audio src={podcast.audioinfo} />
+          <Audio src={podcast.audioinfo} />
         </div>
       </div>
     </div>
