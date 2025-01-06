3ff778a68db28368d7cc1a49975cdfdade6d97c1
Marc Made It <spurdo.dk@gmail.com>
2025-01-06 14:20:50 +0100

    update

--- api/src/main.ts
+++ api/src/main.ts
@@ -2,30 +2,42 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { ValidationPipe } from '@nestjs/common';
 import cookieParser from 'cookie-parser';
-import { join } from 'path';
+// import { join } from 'path';
 import { NestExpressApplication } from '@nestjs/platform-express';
 
 async function bootstrap() {
-  // Create the application as an Express application
   const app = await NestFactory.create<NestExpressApplication>(AppModule);
 
+  // Enable CORS
   app.enableCors({
-    origin: ['http://localhost:5173', 'https://marcmadeit.vercel.app', 'https://mmi.marccode.com'],
+    origin: ['http://localhost:5173', 'https://mmi.marccode.com'],
     credentials: true,
-    methods: ['GET', 'POST', 'DELETE', 'PUT', 'PATCH'],
   });
-
-  // Serve static assets (React frontend)
-  app.useStaticAssets(join(__dirname, '../client'));
-  app.setBaseViewsDir(join(__dirname, '../client'));
-  app.setViewEngine('html');
-
-  // Other configurations
-  app.useGlobalPipes(new ValidationPipe());
   app.setGlobalPrefix('api');
+
+  app.useGlobalPipes(new ValidationPipe());
   app.use(cookieParser());
 
+//   const frontendPath = '/home/marcmadeit/htdocs/mmi.marccode.com/client/dist';
+//   app.useStaticAssets(frontendPath);
+//   app.setBaseViewsDir(frontendPath);
+
+//   app.use('*', (req, res, next) => {
+//   console.log(`Request URL: ${req.originalUrl}`);
+//   if (req.originalUrl.startsWith('/api')) {
+//     console.log('API request, passing to NestJS');
+//     next(); 
+//   } else {
+//     console.log('Frontend request, serving index.html');
+//     res.sendFile(join(frontendPath, 'index.html')); 
+//   }
+// });
+
+
   // Start the application
-  await app.listen(process.env.PORT ?? 3000);
+  const port = process.env.PORT || 8000;
+  await app.listen(port);
+  console.log(`Application running on http://localhost:${port}`);
 }
+
 bootstrap();
