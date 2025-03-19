468676512c4f40720efe47bd6d49dd816f7f7435
Marc Made It <spurdo.dk@gmail.com>
2025-03-19 16:29:31 +0100

    Update vite.config.js

--- client/vite.config.js
+++ client/vite.config.js
@@ -1,7 +1,11 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 
-// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-})
+  build: {
+    rollupOptions: {
+      external: ["quill"], // 🛠️ Sørger for at Quill håndteres korrekt
+    },
+  },
+});
